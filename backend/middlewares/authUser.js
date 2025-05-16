bca492839c7f0991ef86c0d0860bec0906828994
Vlora04 <vloraabllaca@gmail.com>
2025-05-15 22:15:50 -1200

    krijimi i llogaris se pacientit dhe sistemi i regjistrimit

---
+++ backend/middlewares/authUser.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken"
+
+// use authentication middleware
+const authUser = async (req, res, next) => {
+    try {
+
+        const { token } = req.headers
+        if (!token) {
+            return res.json({ success: false, message: 'Nuk jeni të autorizuar, ju lutem kyçuni përsëri!' })
+        }
+        const token_decode = jwt.verify(token, process.env.JWT_SECRET)
+        req.body.userId = token_decode.id
+        next()
+
+
+
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+
+    }
+
+}
+
+export default authUser
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
+import { AppContext } from '../context/AppContext'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
 
+  const { backendUrl, token, setToken } = useContext(AppContext) 
+  const navigate = useNavigate()
   const [state, setState] = useState('Sign Up')
 
   const [email, setEmail] = useState('')
@@ -10,29 +16,60 @@ const Login = () => {
 
   const onSubmitHandler = async (event) => {
     event.preventDefault()
+    try {
+
+      if(state === 'Sign Up'){
+        const {data} = await axios.post(backendUrl + '/api/user/register',{name,email,password})
+        if(data.success){
+          localStorage.setItem('token',data.token)
+          setToken(data.token)
+        } else {
+          toast.error(data.message)
+        }
+      } else {
+          const {data} = await axios.post(backendUrl + '/api/user/login',{email,password})
+        if(data.success){
+          localStorage.setItem('token',data.token)
+          setToken(data.token)
+        } else {
+          toast.error(data.message)
+        }
+
+      }
+     } catch (error){
+      toast.error(error.message)
+    }
   }
 
+
+  useEffect(() =>{
+if(token){
+  navigate('/')
+
+}
+  },[token])
+  
   return (
-    <form className='min-h-[80vh] flex items-center'>
+    <form onSubmit={onSubmitHandler}className='min-h-[80vh] flex items-center'>
       <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg'>
         <p className='text-2xl font-semibold'>{state === 'Sign Up' ? "Krijo Llogarinë" : "Hyr në Llogari"}</p>
         <p>Ju lutem {state === 'Sign Up' ? "regjistrohuni" : "hyni"} për të rezervuar një takim</p>
         {
           state === "Sign Up" && <div className='w-full'>
             <p>Emri dhe Mbiemri</p>
-            <input className='border border-zinc-300 rounded w-full p-2 mt-2' type="text" onChange={(e) => setName(e.target.name)} value={name} required />
+            <input className='border border-zinc-300 rounded w-full p-2 mt-2' type="text" onChange={(e) => setName(e.target.value)} value={name} required />
           </div>
         }
 
         <div className='w-full'>
           <p>Email</p>
-          <input className='border border-zinc-300 rounded w-full p-2 mt-2' type="email" onChange={(e) => setEmail(e.target.email)} value={email} required />
+          <input className='border border-zinc-300 rounded w-full p-2 mt-2' type="email" onChange={(e) => setEmail(e.target.value)} value={email} required />
         </div>
         <div className='w-full'>
           <p>Fjalëkalimi</p>
-          <input className='border border-zinc-300 rounded w-full p-2 mt-2' type="password" onChange={(e) => setPassword(e.target.password)} value={password} required />
+          <input className='border border-zinc-300 rounded w-full p-2 mt-2' type="password" onChange={(e) => setPassword(e.target.value)} value={password} required />
         </div>
-        <button className='bg-primary text-white w-full py-2 rounded-md text-base'>{state === 'Sign Up' ? "Krijo Llogarinë" : "Hyr në Llogari"}</button>
+        <button type='submit' className='bg-primary text-white w-full py-2 rounded-md text-base'>{state === 'Sign Up' ? "Krijo Llogarinë" : "Hyr në Llogari"}</button>
         {
           state === "Sign Up"
             ? <p>
